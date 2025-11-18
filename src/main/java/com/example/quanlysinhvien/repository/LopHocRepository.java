558ea6a96bd586865daf7bff1f6a12b977f26d36
MAP1605 <cuongdvth03230@fpt.edu.vn>
2025-11-18 18:37:33 +0700

    add jpa

---
+++ src/main/java/com/example/quanlysinhvien/repository/LopHocRepository.java
@@ -0,0 +1,7 @@
+package com.example.quanlysinhvien.repository;
+
+import com.example.quanlysinhvien.entity.LopHoc;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LopHocRepository extends JpaRepository<LopHoc, Integer> {
+}
---
+++ src/main/java/com/example/quanlysinhvien/repository/SinhVienRepository.java
@@ -0,0 +1,7 @@
+package com.example.quanlysinhvien.repository;
+
+import com.example.quanlysinhvien.entity.SinhVien;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SinhVienRepository extends JpaRepository<SinhVien, Integer> {
+}
