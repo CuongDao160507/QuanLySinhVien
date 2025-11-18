b2e62a8a300a9491e180abc39cf40f2878d53f05
MAP1605 <cuongdvth03230@fpt.edu.vn>
2025-11-19 03:05:58 +0700

    controller/thymeleaf - add

--- src/main/java/com/example/quanlysinhvien/controller/SinhVienController.java
+++ src/main/java/com/example/quanlysinhvien/controller/SinhVienController.java
@@ -1,11 +1,18 @@
 package com.example.quanlysinhvien.controller;
 
+import com.example.quanlysinhvien.entity.LopHoc;
+import com.example.quanlysinhvien.entity.SinhVien;
+import com.example.quanlysinhvien.repository.LopHocRepository;
 import com.example.quanlysinhvien.service.SinhVienService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/sinh-vien")
@@ -13,9 +20,24 @@ public class SinhVienController {
     @Autowired
     SinhVienService sinhVienService;
 
+    @Autowired
+    LopHocRepository lopHocRepository;
+
+    @ModelAttribute("listLH")
+    public List<LopHoc> findAllLopHoc() {
+        return lopHocRepository.findAll();
+    }
+
     @GetMapping("/display")
     public String display(Model model) {
         model.addAttribute("listSV", sinhVienService.findAllSinhVien());
+        model.addAttribute("sv", new SinhVien());
         return "sinhVien/display";
+    }
+
+    @PostMapping("/add")
+    public String add(@ModelAttribute("sv") SinhVien sinhVien) {
+        sinhVienService.addSinhVien(sinhVien);
+        return "redirect:/sinh-vien/display";
     }
 }
