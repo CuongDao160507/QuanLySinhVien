55318ef95db35df216bde3f1a1ae64bcfc98f610
MAP1605 <cuongdvth03230@fpt.edu.vn>
2025-11-19 02:12:59 +0700

    controller - display

--- src/main/java/com/example/quanlysinhvien/controller/SinhVienController.java
+++ src/main/java/com/example/quanlysinhvien/controller/SinhVienController.java
@@ -1,4 +1,21 @@
 package com.example.quanlysinhvien.controller;
 
+import com.example.quanlysinhvien.service.SinhVienService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sinh-vien")
 public class SinhVienController {
+    @Autowired
+    SinhVienService sinhVienService;
+
+    @GetMapping("/display")
+    public String display(Model model) {
+        model.addAttribute("listSV", sinhVienService.findAllSinhVien());
+        return "sinhVien/display";
+    }
 }
